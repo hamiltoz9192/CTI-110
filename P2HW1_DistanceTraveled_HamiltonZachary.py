e8374f1cdaaf89b48a8199b790660e2d13ce3453
hamiltoz9192 <40342542+hamiltoz9192@users.noreply.github.com>
2018-06-17 22:48:42 +0300

    Add files via upload
    First Submission

---
+++ P2HW1_DistanceTraveled_HamiltonZachary.py
@@ -0,0 +1,22 @@
+# CTI-110
+# P2HW1 - Distance Traveled
+# Zachary Hamilton
+# June 17, 2018
+# This program shows the distance a car has traveled when going 70 mph.
+speed = 70
+distanceAfter6 = 6
+distanceAfter10 = 10
+distanceAfter15 = 15
+
+#Speed of the car
+print("A car is traveling at 70 mph")
+
+#The distance the car will travel in 6 hours.
+print("distance the car will travel in 6 hours is", speed * distanceAfter6, "miles")
+
+#The distance the car will travel in 10 hours.
+print("distance the car will travel in 10 hours is", speed * distanceAfter10, "miles")
+
+#The distance the car will travel in 15 hours.
+print("distance the car will travel in 15 hours is", speed * distanceAfter15, "miles")
+
